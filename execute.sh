240e1f6efaab66ee263bc093444ab2ea9806d983
Nagesh M H <n4smh@outlook.com>
2021-11-15 12:10:05 +0530

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -4,4 +4,8 @@ echo "----------------------------------------------"
 echo "Current directory: $PWD"
 DIR_CONTENT=$(ls -la)
 echo "Current directory content: $DIR_CONTENT"
+GitVersion=$(git --version)
+echo "Git version $GitVersion"
+JavaVersion =$(java -version)
+echo "Java version $JavaVersion"
 echo "---------------------------------------------"
