ec766edc9419b561a5c98425dcc57d11cf0dc76b
Nagesh M H <n4smh@outlook.com>
2021-11-15 11:53:24 +0530

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "----------------------------------------------"
 echo "Current directory: $PWD"
 DIR_CONTENT=ls -la
 echo "Current directory content: $DIR_CONTENT"
+echo "----------------------------------------------"
